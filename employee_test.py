0b402d154660b95447988fb7cf28cf0a274f863a
Jeroen Keppens <jeroen.keppens@kcl.ac.uk>
2022-10-03 01:28:12 +0100

    Update tests
    Allow 1 or more spaces between sentences.  The original version required two spaces.

--- employee_test.py
+++ employee_test.py
@@ -2,33 +2,40 @@
 """DO NOT CHANGE THIS FILE!"""
 
 from employee import *
+import re
 
 def test_billie():
     assert billie.get_pay() == 4000
-    string = 'Billie works on a monthly salary of 4000.  Their total pay is 4000.'
-    assert str(billie) == string
+    string = str(billie)
+    regex = '^Billie works on a monthly salary of 4000.\s+Their total pay is 4000.$'
+    assert re.match(regex, string)
 
 def test_charlie():
     assert charlie.get_pay() == 2500
-    string = 'Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.'
-    assert str(charlie) == string
+    string = str(charlie)
+    regex = '^Charlie works on a contract of 100 hours at 25/hour.\s+Their total pay is 2500.$'
+    assert re.match(regex, string)
 
 def test_renee():
     assert renee.get_pay() == 3800
-    string = 'Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.'
-    assert str(renee) == string
+    string = str(renee)
+    regex = 'Renee works on a monthly salary of 3000 and receives a commission for 4 contract\(s\) at 200/contract.\s+Their total pay is 3800.'
+    assert re.match(regex, string)
 
 def test_jan():
     assert jan.get_pay() == 4410
-    string  = 'Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.'
-    assert str(jan) == string
+    string = str(jan)
+    regex  = 'Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract\(s\) at 220/contract.\s+Their total pay is 4410.'
+    assert re.match(regex, string)
 
 def test_robbie():
     assert robbie.get_pay() == 3500
-    string = 'Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.'
-    assert str(robbie) == string
+    string = str(robbie)
+    regex = '^Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.\s+Their total pay is 3500.$'
+    assert re.match(regex, string)
 
 def test_ariel():
     assert ariel.get_pay() == 4200
-    string = 'Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.'
-    assert str(ariel) == string
+    string = str(ariel)
+    regex = '^Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.\s+Their total pay is 4200.$'
+    assert re.match(regex, string)
